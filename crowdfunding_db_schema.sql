b0e485fa50f22bd8e4b3da60490d140098847445
isabellajade <isabellajadet@gmail.com>
2023-01-30 21:52:52 -0600

    database images

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,48 +1,57 @@
-DROP TABLE IF EXISTS category cascade;
-
-CREATE TABLE category (
-    category_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    category VARCHAR(50) NOT NULL
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
-DROP TABLE IF EXISTS subcategory cascade;
-
-CREATE TABLE subcategory (
-    subcategory_id VARCHAR(20) PRIMARY KEY NOT NULL,
-    subcategory VARCHAR(50) NOT NULL
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(20)   NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
-DROP TABLE IF EXISTS contacts cascade;
-
-CREATE TABLE contacts (
-    contact_id INT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-	last_name VARCHAR(20) NOT NULL,
-	email VARCHAR(80) NOT NULL
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "email" VARCHAR(80)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
-DROP TABLE IF EXISTS campaign cascade;
-
-CREATE TABLE campaign (
-    cf_id INT PRIMARY KEY NOT NULL,
-	contact_id INT NOT NULL,
-		FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
-	company_name VARCHAR(80) NOT NULL,
-	description VARCHAR(150) NOT NULL,
-	goal INT NOT NULL,
-	pledged INT NOT NULL,
-	outcome VARCHAR(15) NOT NULL,
-	backers_count INT NOT NULL,
-    country VARCHAR(5) NOT NULL,
-	currency VARCHAR(5) NOT NULL,
-	launch_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	category_id VARCHAR(10) NOT NULL,
-		FOREIGN KEY (category_id) REFERENCES category(category_id),
-	subcategory_id VARCHAR(20) NOT NULL,
-		FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" NUMERIC(10,2)   NOT NULL,
+    "pledged" NUMERIC(10,2)   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
 );
 
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
 
 
 
